a4be9939fd45104f7ccc025837bc8105733ad5c4
Swati Mandal <Swatimandal01@gmail.com>
2021-10-22 14:36:46 +0530

    done
    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router , Route ,Switch  } from 'react-router-dom';
 import Nav from './Component/Nav';
 import Engineering from './Component/Engineering/Acoustics/Acoustics';
+import Acoustics from './Component/Engineering/Acoustics/Acoustics';
+import Fitness from './Component/Engineering/Fitness/Fitness';
+import FMEA from './Component/Engineering/FMEA/Fmea';
+import Integrity from './Component/Engineering/Intergrity/Intergrity';
+
 
 
 
@@ -10,9 +15,27 @@ function App(){
   return(
     <Router>
       <Nav />
+
       <Switch>
-        <Route>
+        <Route path="/abc">
         <Engineering />
+      
+        </Route>
+        <Route path="/def">
+        <Acoustics/>
+      
+        </Route>
+        <Route path="/qw">
+        < Fitness/>
+      
+        </Route>
+        <Route path="/rf">
+        <FMEA />
+      
+        </Route>
+        <Route path="/yt">
+        <Integrity />
+      
         </Route>
       </Switch>
     </Router>
--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -4,61 +4,60 @@ import {BrowserRouter, Link} from "react-router-dom";
 
 
 function Nav() {
-  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
 
   return (
-    <ReactBootStrap.Nav variant="pills" activeKey="1" onSelect={handleSelect} style={{background: "crimson", color:"white"}}>
+    <ReactBootStrap.Nav variant="pills"  style={{background: "crimson", color:"white"}}>
        <Link to="">
        <ReactBootStrap.NavDropdown title="Products" id="nav-dropdown" >
-        <ReactBootStrap.NavDropdown.Item eventKey="1.1">CMX Sentinel</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="1.2">CMX Stream</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >CMX Sentinel</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >CMX Stream</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
       </Link>
 
       <Link to=""   >
       <ReactBootStrap.NavDropdown title="Trendius" id="nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item eventKey="2.1">Predective Analytics</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="2.2">Performance Analytics</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Predective Analytics</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Performance Analytics</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
       </Link>
 
       <Link to ="">
       <ReactBootStrap.NavDropdown title="Engineering" id="nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item eventKey="3.1">Acoustics & Pulsation</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="3.2">Vibration and Noise</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="3.3">FMEA</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="3.4">Intergrity & Reliability</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="3.4">Surge & Fluid Dynamics</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="3.4">Fitness for Services</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Acoustics & Pulsation</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Vibration and Noise</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >FMEA</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Intergrity & Reliability</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Surge & Fluid Dynamics</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Fitness for Services</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
       </Link>
       
       <Link to ="">
       <ReactBootStrap.NavDropdown title="PlantMORE" id="nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item eventKey="4.1">Turnaround</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="4.2">Balancing & Alignment</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="4.3">O&M</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="4.4">Commisioning and Startup</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="4.4">ESP</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="4.4">Field Condition Monitoring</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Turnaround</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Balancing & Alignment</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >O&M</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Commisioning and Startup</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >ESP</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Field Condition Monitoring</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
       </Link>
 
       <Link to ="">
       <ReactBootStrap.NavDropdown title="Prespectives" id="nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item eventKey="5.1">Whitepaper</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="5.2">Resources</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="5.3">Blogs</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Whitepaper</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Resources</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Blogs</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
       </Link>
 
       <Link to ="">
       <ReactBootStrap.NavDropdown title="Company" id="nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item eventKey="6.1">About Us</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="6.2">Leadership</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="6.3">Policies</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="6.4">Sustanibility</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item eventKey="6.4">Careers</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >About Us</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Leadership</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Policies</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Sustanibility</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item >Careers</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
       </Link>
       
